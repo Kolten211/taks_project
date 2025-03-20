3c30aad64a3cd36c20f4d7a62f09580817e76525
Kolten <kolten.smith1@gmail.com>
2025-03-19 19:32:27 -0600

    Finished part 0

---
+++ Backend/config/index.js
@@ -0,0 +1,44 @@
+// backend/config/index.js
+module.exports = {
+    environment: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 8000,
+    dbFile: process.env.DB_FILE,
+    jwtConfig: {
+      secret: process.env.JWT_SECRET,
+      expiresIn: process.env.JWT_EXPIRES_IN
+    }
+  };
+
+// const { environment } = require('./config');
+// const isProduction = environment === 'production';
+
+// const app = express();
+
+// app.use(morgan('dev'));
+
+// app.use(cookieParser());
+// app.use(express.json());
+
+// // Security Middleware
+// if (!isProduction) {
+//     // enable cors only in development
+//     app.use(cors());
+//   }
+  
+//   // helmet helps set a variety of headers to better secure your app
+//   app.use(
+//     helmet.crossOriginResourcePolicy({
+//       policy: "cross-origin"
+//     })
+//   );
+  
+//   // Set the _csrf token and create req.csrfToken method
+//   app.use(
+//     csurf({
+//       cookie: {
+//         secure: isProduction,
+//         sameSite: isProduction && "Lax",
+//         httpOnly: true
+//       }
+//     })
+//   );
